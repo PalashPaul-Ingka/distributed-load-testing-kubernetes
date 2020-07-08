57917f7c5124f2f4cdd77796ea1506b8baf4b86d
Palash Paul <palash.paul@ingka.com>
2020-07-08 10:32:51 +0200

    v1

---
+++ docker-image/locust-tasks/products.py
@@ -0,0 +1,23 @@
+def get_products():
+    return [
+        {"countryCode":"RU","langCode":"ru","productId":"00014894"},
+        {"countryCode":"JO","langCode":"ar","productId":"00015134"},
+        {"countryCode":"KW","langCode":"ar","productId":"00015134"},
+        {"countryCode":"MA","langCode":"ar","productId":"00015134"},
+        {"countryCode":"SA","langCode":"ar","productId":"00015134"},
+        {"countryCode":"ES","langCode":"ca","productId":"00028508"},
+        {"countryCode":"HR","langCode":"hr","productId":"00028508"},
+        {"countryCode":"DK","langCode":"da","productId":"00015134"},
+        {"countryCode":"ES","langCode":"en","productId":"00058487"},
+        {"countryCode":"ES","langCode":"eu","productId":"00058487"},
+        {"countryCode":"ES","langCode":"eu","productId":"00100467"},
+        {"countryCode":"AE","langCode":"en","productId":"00015134"},
+        {"countryCode":"BH","langCode":"en","productId":"00015134"},
+        {"countryCode":"GB","langCode":"en","productId":"00103343"},
+        {"countryCode":"IT","langCode":"it","productId":"00103343"},
+        {"countryCode":"SE","langCode":"en","productId":"09343720"},
+        {"countryCode":"JO","langCode":"en","productId":"00015134"},
+        {"countryCode":"KW","langCode":"en","productId":"00015134"},
+        {"countryCode":"MA","langCode":"en","productId":"00015134"},
+        {"countryCode":"SE","langCode":"en","productId":"09343744"}
+    ]
